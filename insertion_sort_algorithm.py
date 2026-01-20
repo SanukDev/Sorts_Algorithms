b766e7e8ba597f11643d96c456c3b0eff55393a7
Samuel Rodrigues Melo <152120362+SanukDev@users.noreply.github.com>
2026-01-20 01:16:39 -0300

    insertion sort complete

--- insertion_sort_algorithm.py
+++ insertion_sort_algorithm.py
@@ -1,10 +1,16 @@
 arr = [5,2,3,7,4,8,6]
+cont = 1
 for i in range(1,len(arr)):
     print(arr)
-    if arr[i-1] > arr[i]:
-        num = arr[i-1]
-        arr[i-1] = arr[i]
-        arr[i] = num
+    cont = i
+    print(cont)
+    while arr[cont] < arr[cont-1]:
+        num = arr[cont-1]
+        arr[cont-1] = arr[cont]
+        arr[cont] = num
+        cont -= 1
+        if cont == 0:
+            break
 
 
 print(arr)
