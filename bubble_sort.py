fe4730c52fa51dc8185252e6cf7e5a52354d5299
Samuel Rodrigues Melo <152120362+SanukDev@users.noreply.github.com>
2026-01-28 00:05:47 -0300

    Finish the Bubble Sort Algorithm

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,10 @@
 arr = [5,2,3,7,4,8,6]
 
 for i in range(len(arr)):
-    for j in range(len(arr)):
-        if arr[i] < arr[j]:
-            num = arr[i]
-            arr[i] = arr[j]
-            arr[j] = num
+    for j in range(len(arr)-1):
+        if arr[j] > arr[j+1]:
+            num = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = num
+            break
         print(arr)
