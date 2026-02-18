e2192a0a95ef984cdd8e329519bf2192fc88e94b
Samuel Rodrigues Melo <152120362+SanukDev@users.noreply.github.com>
2026-02-17 22:06:23 -0300

    Create a function bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,13 @@
 arr = [5,2,3,7,4,8,6]
 
-for i in range(len(arr)):
-    for j in range(len(arr)-1):
-        if arr[j] > arr[j+1]:
-            num = arr[j]
-            arr[j] = arr[j+1]
-            arr[j+1] = num
-            break
-        print(arr)
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                num = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = num
+                break
+            print(arr)
+
+bubble_sort(arr=arr)
